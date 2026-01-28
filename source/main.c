9b5db756c11974d4ec1551e885a6c862e3a32149
mesyeti <mesyeti@mesyeti.uk>
2026-01-28 17:43:09 +0000

    scene guide

--- source/main.c
+++ source/main.c
@@ -1,4 +1,5 @@
 #include <Arkas/engine.h>
+#include "scene.h"
 
 int main(void) {
 	Engine_Init("Arkas Template");
@@ -6,6 +7,9 @@ int main(void) {
 	// usually you would add the first scene here, instead i'm just enabling
 	// the console
 	engine.console = true;
+
+	// here's how you would add a scene:
+	// SceneManager_AddScene(TemplateScene());
 
 	while (engine.running) {
 		Engine_Update();
